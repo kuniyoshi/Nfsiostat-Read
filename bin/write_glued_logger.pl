6d3478090f3a892efd91fb7e06502bb36e9f1aac
Kuniyoshi Kouji <kuniyoshi@seesaa.co.jp>
2015-03-02 20:20:59 +0900

    add one shot script.

---
+++ bin/write_glued_logger.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+use 5.10.0;
+use utf8;
+use strict;
+use warnings;
+use open qw( :std :utf8 );
+use autodie qw( open close );
+use Data::Dumper;
+
+$Data::Dumper::Terse    = 1;
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent   = 1;
+
+chomp( my @files = `find lib -type f -name \\*.pm` );
+
+my $glued_code;
+
+for my $file ( @files ) {
+    open my $FH, "<", $file;
+
+    my $data = do { local $/; <$FH> };
+    $data =~ s{
+        ^ __END__ $
+        .*
+    }{}msx;
+
+    for my $used_file ( @files ) {
+        ( my $module_name = $used_file ) =~ s{lib/}{};
+        $module_name =~ s{ [/] }{::}msx;
+        $module_name =~ s{ [.]pm \z}{}msx;
+        $data =~ s{
+            ^ use \s \Q$module_name\E .*? $
+        }{}msx;
+    }
+
+    $glued_code .= $data;
+
+    close $FH;
+}
+
+my $header = <<'END_HEADER';
+#!/usr/bin/perl -s
+use 5.10.0;
+use strict;
+use warnings;
+
+END_HEADER
+
+my $body = <<'END_BODY';
+package main;
+our $interval  ||= 2;
+our $iteration ||= 22;
+
+my $count;
+
+my $reader = Nfsiostat::Read->new;
+
+while ( $count++ < $iteration ) {
+    say $_
+        for $reader->load->parse->make_logs;
+
+    sleep $interval;
+}
+
+exit;
+
+END_BODY
+
+print $header, $glued_code, "\n", $body;
+
+exit;
---
+++ script/nfsiostat_logger.pl
@@ -0,0 +1,221 @@
+#!/usr/bin/perl -s
+use 5.10.0;
+use strict;
+use warnings;
+
+use 5.10.0;
+use strict;
+use warnings;
+package Nfsiostat::Read;
+use autodie qw( open close );
+use Readonly;
+
+our $VERSION = "0.01";
+
+Readonly my $PROC_FILE       => "/proc/self/mountstats";
+Readonly my $DEVICE_PARSE_RE => qr{
+    \A
+    device \s
+    (?<name>[^\s]+) \s
+    mounted \s on \s
+    (?<mounted_on>[^\s]+) \s
+    with \s fstype \s
+    (?<fstype>[^\s]+)
+}msx;
+Readonly my $PER_OP_RE       => qr{
+    \A
+    \t [ ]+
+    (?<action>\w+)[:] \s
+    (?<operations>\d+) \s
+    (?<transmissions>\d+) \s
+    (?<major_timeouts>\d+) \s
+    (?<bytes_sent>\d+) \s
+    (?<bytes_received>\d+) \s
+    (?<cumulative_queue_ms>\d+) \s
+    (?<cumulative_response_ms>\d+) \s
+    (?<cumulative_total_request_ms>\d+)
+    \z
+}msx;
+Readonly our @FIELD_NAMES        => qw(
+    operations
+    transmissions
+    major_timeouts
+    bytes_sent
+    bytes_received
+    cumulative_queue_ms
+    cumulative_response_ms
+    cumulative_total_request_ms
+);
+Readonly our @VALID_ACTIONS      => qw(
+	NULL
+	GETATTR
+	SETATTR
+	LOOKUP
+	ACCESS
+	READLINK
+	READ
+	WRITE
+	CREATE
+	MKDIR
+	SYMLINK
+	MKNOD
+	REMOVE
+	RMDIR
+	RENAME
+	LINK
+	READDIR
+	READDIRPLUS
+	FSSTAT
+	FSINFO
+	PATHCONF
+);
+Readonly our @INTERESTED_ACTIONS => qw(
+    GETATTR
+    SETATTR
+    LOOKUP
+    ACCESS
+    READ
+    WRITE
+    READDIR
+    READDIRPLUS
+    FSSTAT
+);
+
+sub new {
+    my $class = shift;
+    my %param = @_;
+    my $self = bless { interested_actions => delete $param{interested_actions} }, $class;
+    return $self;
+}
+
+sub load {
+    my $self     = shift;
+    my $filename = shift || $PROC_FILE;
+    open my $FH, "<", $filename;
+    chomp( my @lines = <$FH> );
+    $self->{_lines} = \@lines;
+    close $FH;
+    return $self;
+}
+
+sub parse {
+    my $self = shift;
+    my %device;
+    my %statistics;
+    my $age;
+
+    for my $line ( @{ $self->{_lines} } ) {
+
+        if ( $line =~ m{$DEVICE_PARSE_RE} ){
+            %device = %+;
+            next;
+        }
+
+        if ( $line =~ m{\A \s+ age: \s+ (?<age>\d+) \z}msx ) {
+            $age = $+{age};
+        }
+
+        if ( $line =~ m{$PER_OP_RE} ) {
+            my %stat = %+;
+            my $action = delete $stat{action};
+            $statistics{ $device{name} }{ $action } = \%stat;
+            next;
+        }
+    }
+
+    $statistics{age} = $age;
+
+    $self->{_statistics} = \%statistics;
+
+    return $self;
+}
+
+sub interested_actions {
+    my $self = shift;
+    return @{ $self->{interested_actions} || [ ] } || @INTERESTED_ACTIONS;
+}
+
+sub make_logs {
+    my $self = shift;
+    my %statistics = %{ $self->{_statistics} };
+    my $age = $statistics{age};
+    my @devices = grep { $_ ne "age" } keys %statistics;
+    my @logs;
+
+    for my $device ( @devices ) {
+        for my $action ( keys %{ $statistics{ $device } } ) {
+            next
+                if !grep { $_ eq $action } $self->interested_actions;
+            push @logs, join "\t", $age, $device, $action, @{ $statistics{ $device }{ $action } }{ @FIELD_NAMES };
+        }
+    }
+
+    return @logs;
+}
+
+1;
+
+use 5.10.0;
+use strict;
+use warnings;
+package Nfsiostat::Read::Log;
+use Readonly;
+use List::MoreUtils qw( mesh );
+
+
+our $VERSION = "0.01";
+
+my %PREVIOUS_STAT;
+
+sub parse {
+    my $class = shift;
+    my $line  = shift;
+
+    my( $age, $device, $action, @fields ) = split m{\t}, $line;
+    my %stat = mesh( @Nfsiostat::Read::FIELD_NAMES, @fields );
+
+  DIFF_WITH_PREVIOUS:
+    for my $name ( @Nfsiostat::Read::FIELD_NAMES ) {
+        if ( !exists $PREVIOUS_STAT{ $device }{ $action }{ $name }{age} ) {
+            @{ $PREVIOUS_STAT{ $device }{ $action }{ $name } }{ qw( age value ) } = ( $age, $stat{ $name } );
+            next;
+        }
+
+        $stat{ $name } = $PREVIOUS_STAT{ $device }{ $action }{ $name }{value}
+        / ( $age - $PREVIOUS_STAT{ $device }{ $action }{ $name }{age} );
+    }
+
+    return
+        if $PREVIOUS_STAT{ $device }{ $action }{operations}{age} == $age;
+
+    $stat{avg_queue_ms}    = $stat{transmissions} / $stat{cumulative_queue_ms};
+    $stat{avg_response_ms} = $stat{transmissions} / $stat{cumulative_response_ms};
+    $stat{avg_request_ms}  = $stat{operations} / $stat{cumulative_total_request_ms};
+
+  CHANGE_CURRENT_TO_PREVIOUS:
+    for my $name ( @Nfsiostat::Read::FIELD_NAMES ) {
+        @{ $PREVIOUS_STAT{ $device }{ $action }{ $name } }{ qw( age value ) } = ( $age, $stat{ $name} );
+    }
+
+    return ( device => $device, action => $action, stat => \%stat );
+}
+
+1;
+
+package main;
+our $interval  ||= 2;
+our $iteration ||= 22;
+
+my $count;
+
+my $reader = Nfsiostat::Read->new;
+
+while ( $count++ < $iteration ) {
+    say $_
+        for $reader->load->parse->make_logs;
+
+    sleep $interval;
+}
+
+exit;
+
